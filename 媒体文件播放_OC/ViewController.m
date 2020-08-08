f8925c389ef0210118c0ae4fc62ddea1c074de02
SpaceStar2016 <570175236@qq.com>
2020-08-08 16:40:11 +0800

    播放mp4

--- 媒体文件播放_OC/ViewController.m
+++ 媒体文件播放_OC/ViewController.m
@@ -7,16 +7,32 @@
 //
 
 #import "ViewController.h"
-
+#import <AVKit/AVKit.h>
+//#import <AVFoundation/AVFoundation.h>
 @interface ViewController ()
-
+@property(nonatomic,strong)AVPlayerViewController * playViewVC;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.playViewVC = [[AVPlayerViewController alloc] init];
+//    NSString * abPath = @"/Users/spacezhong/Desktop/Video/试讲00.mov";
+    NSString * paaa = [[NSBundle mainBundle] pathForResource:@"src" ofType:nil];
+    NSString * newpath = [NSString stringWithFormat:@"%@/video/笑傲江湖MP4.mp4",paaa];
+    NSURL * fileUrl = [NSURL fileURLWithPath:newpath];
+    AVPlayer * play = [AVPlayer playerWithURL:fileUrl];
+    self.playViewVC.player = play;
+    self.playViewVC.view.frame = self.view.bounds;
+    self.playViewVC.view.backgroundColor = [UIColor blackColor];
+    self.playViewVC.showsPlaybackControls = YES;
+    [self.view addSubview:self.playViewVC.view];
+    [self.playViewVC.player play];
+
+    
 }
 
 
--- 媒体文件播放_Swift/ViewController.swift
+++ 媒体文件播放_Swift/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
-
+import AVKit
 class ViewController: UIViewController {
-
+    
+    var playViewVC:AVPlayerViewController!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpPlayView()
+        playViewVC.player?.play()
+//        playViewVC =
         // Do any additional setup after loading the view.
+    }
+    
+    
+    func setUpPlayView(){
+        guard let url = Bundle.main.path(forResource:"src", ofType:"")else{return}
+        let newUlr = url + "/video/笑傲江湖MP4.mp4"
+        let realUrl:NSURL = NSURL(fileURLWithPath: newUlr)
+        playViewVC = AVPlayerViewController()
+        playViewVC.player = AVPlayer(url: realUrl as URL)
+        playViewVC.view.frame = view.bounds
+        view.addSubview(playViewVC.view)
     }
 
 
